2ed9dc589ec56fc0cd9bdc963913edb4dfc1d3fb
idith almog <almog.idith@gmail.com>
2020-04-30 21:16:41 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,11 +2,12 @@
 def prime?(number)
   i=2
   while i <= number/2
-    if number % i ==0
+    if number % i == 0
       return false
     else
       i +=1
+      true
     end
   end
-  true
+
 end
