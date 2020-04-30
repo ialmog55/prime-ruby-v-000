70f2f7481ccad795369fb254941010b29518d1bd
idith almog <almog.idith@gmail.com>
2020-04-30 21:22:56 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,13 +1,14 @@
 # Add  code here!
 def prime?(number)
-  if number <= 1 return false
+  if number <= 1 
+    return false
   end
   i=2
   while i <= number/2
     if number % i == 0
       return false
     else
-      i +=1
+      i += 1
     end
     true
   end
